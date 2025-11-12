0a7092aa55a6ec0397cd85b1be5f7092ac0ff810
siwakasen <siwakasen@gmail.com>
2025-11-12 13:58:18 +0700

    test: logging env

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,8 +1,8 @@
-import { connection } from "next/server";
 import { envConfig } from "./configs/env.config";
 
 export async function register() {
   envConfig.recaptchaSiteKey = String(
     process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY
   );
+  console.log("recaptchaSiteKey", envConfig.recaptchaSiteKey);
 }
