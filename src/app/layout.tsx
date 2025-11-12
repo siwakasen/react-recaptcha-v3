7e1c7d4501178df3fb6957b86b4dcdc47e89c9f4
siwakasen <siwakasen@gmail.com>
2025-11-12 11:36:12 +0700

    fix: remove use server on layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-"use server";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -18,7 +17,7 @@ export const metadata: Metadata = {
   description: "Google reCAPTCHA Token",
 };
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
