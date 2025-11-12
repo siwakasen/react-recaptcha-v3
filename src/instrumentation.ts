c2bf204137a2699c309c12687334f24fe496e74b
siwakasen <siwakasen@gmail.com>
2025-11-12 17:01:50 +0700

    fix: env variable bind on build args

--- src/instrumentation.ts
+++
@@ -1,8 +0,0 @@
-import { envConfig } from "./configs/env.config";
-
-export async function register() {
-  envConfig.recaptchaSiteKey = String(
-    process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY
-  );
-  console.log("recaptchaSiteKey", envConfig.recaptchaSiteKey);
-}
