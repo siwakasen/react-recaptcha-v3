a63160a2f0cec5c6163a17acb4785a4c32a94a85
siwakasen <siwakasen@gmail.com>
2025-12-12 23:46:00 +0700

    fix: security issue CVE-2025-66478

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   reactCompiler: true,
+  allowedDevOrigins: ["192.168.20.205"],
 };
 
 export default nextConfig;
