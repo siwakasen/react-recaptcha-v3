b0fbb0b6f45a0485eda2652a852e2b6b3e6d3db1
siwakasen <siwakasen@gmail.com>
2025-12-12 23:59:18 +0700

    fix: remove devOrigin

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   reactCompiler: true,
-  allowedDevOrigins: ["192.168.20.205"],
 };
 
 export default nextConfig;
